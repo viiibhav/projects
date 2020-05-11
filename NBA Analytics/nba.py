2a35b1f57f79b04ddebd4fc7e20e0e9f55eebe0b
Vibhav Dabadghao <vibhav.dabadghao@gmail.com>
2020-05-11 14:08:49 -0400

    Accounted for differences in csv format across years

--- NBA Analytics/nba.py
+++ NBA Analytics/nba.py
@@ -9,11 +9,19 @@ import numpy as np
 import pandas as pd
 
 filename = 'nba_seasons\leagues_NBA_{}_games_games.csv'
-cols = [0, 1, 3, 4, 5, 6, 7, 15, 17]
-names = ['Date', 'Start (ET)', 'Visitor/Neutral', 'V/N points',
-         'Home/Neutral', 'H/N points', 'OT', 'V/N ID', 'H/N ID']
+cols = [0, 3, 4, 5, 6, 15, 17]
+names = ['Date', 'Visitor/Neutral', 'V/N points', 'Home/Neutral', 'H/N points', 'V/N ID', 'H/N ID']
 years = np.arange(2001, 2019)
-df = pd.DataFrame({name: [] for name in names})
-for year in years:
-    df1 = pd.read_csv(filename.format(year), usecols=cols, names=names)
-    df = pd.concat([df, df1])
+df = {year: pd.DataFrame({name: [] for name in names}) for year in years}
+for year in years[:-3]:
+    df[year] = pd.read_csv(filename.format(year), header=0, usecols=cols,
+                           names=names, keep_default_na=False)
+
+cols = [0, 1, 2, 3, 4, 15, 17]
+teams = {}
+for year in years[-3:]:
+    df[year] = pd.read_csv(filename.format(year), header=0, nrows=1230,
+                           usecols=cols, names=names, keep_default_na=False)
+    teams[year] = pd.read_csv(filename.format(year), header=None, skiprows=1, nrows=30,
+                              usecols=[8, 9, 10],
+                              names=['Team', 'Team Code', 'TeamID'])
