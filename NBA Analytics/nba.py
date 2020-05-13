055a96ccc17caf9107628e932c4d6346482b9c73
Vibhav Dabadghao <vibhav.dabadghao@gmail.com>
2020-05-13 11:36:24 -0400

    Fixed issues related to changes in franchise names

--- NBA Analytics/nba.py
+++ NBA Analytics/nba.py
@@ -10,42 +10,42 @@ import pandas as pd
 
 filename = 'nba_seasons\leagues_NBA_{}_games_games.csv'
 
-cols = [0, 3, 4, 5, 6, 15, 17]
-names = ['Date', 'Visitor/Neutral', 'V/N points', 'Home/Neutral', 'H/N points', 'V/N ID', 'H/N ID']
-years = np.arange(2001, 2019)
-years = np.delete(years, [1, 12])
+cols = [0, 3, 4, 5, 6]
+names = ['Date', 'Visitor/Neutral', 'V/N points', 'Home/Neutral', 'H/N points']
+years = np.arange(2005, 2019)
 
 df = {year: pd.DataFrame({name: [] for name in names}) for year in years}
 for year in years[:-3]:
     df[year] = pd.read_csv(filename.format(year), header=0, usecols=cols,
                            names=names, keep_default_na=False)
-    df[year]['Date'] = pd.to_datetime(df[year]['Date'])
+    df[year]['Date'] = pd.to_datetime(df[year]['Date']).dt.date
 
 # Seasons 2016 -- 2018 have different formats
-cols = [0, 1, 2, 3, 4, 15, 17]
+cols = [0, 1, 2, 3, 4]
 for year in years[-3:]:
     df[year] = pd.read_csv(filename.format(year), header=0, nrows=1230,
                            usecols=cols, names=names, keep_default_na=False)
-    df[year]['Date'] = pd.to_datetime(df[year]['Date'])
+    df[year]['Date'] = pd.to_datetime(df[year]['Date']).dt.date
 
 # List of teams
-teams = pd.read_csv(filename.format(2018), header=None, skiprows=1, nrows=30,
+teams = pd.read_csv(filename.format(2018), header=None, skiprows=1, nrows=35,
                     usecols=[8, 9, 10], names=['Team', 'Team Code', 'TeamID'],
                     index_col='Team')
 
 # Team Divisions
-divisions = {'Atlantic': ['Boston Celtics', 'Brooklyn Nets', 'New York Knicks',
-                          'Philadelphia 76ers', 'Toronto Raptors'],
+divisions = {'Atlantic': ['Boston Celtics', 'Brooklyn Nets', 'New Jersey Nets',
+                          'New York Knicks', 'Philadelphia 76ers', 'Toronto Raptors'],
              'Central': ['Chicago Bulls', 'Cleveland Cavaliers', 'Detroit Pistons',
                          'Indiana Pacers', 'Milwaukee Bucks'],
-             'Southeast': ['Atlanta Hawks', 'Charlotte Hornets', 'Miami Heat', 'Orlando Magic',
-                           'Washington Wizards'],
+             'Southeast': ['Atlanta Hawks', 'Charlotte Hornets', 'Charlotte Bobcats',
+                           'Miami Heat', 'Orlando Magic', 'Washington Wizards'],
              'Northwest': ['Denver Nuggets', 'Minnesota Timberwolves', 'Oklahoma City Thunder',
-                           'Portland Trail Blazers', 'Utah Jazz'],
+                           'Portland Trail Blazers', 'Utah Jazz', 'Seattle SuperSonics'],
              'Pacific': ['Golden State Warriors', 'Los Angeles Clippers', 'Los Angeles Lakers',
                          'Phoenix Suns', 'Sacramento Kings'],
              'Southwest': ['Dallas Mavericks', 'Houston Rockets', 'Memphis Grizzlies',
-                           'New Orleans Pelicans', 'San Antonio Spurs']}
+                           'New Orleans Pelicans', 'New Orleans Hornets',
+                           'New Orleans/Oklahoma City Hornets', 'San Antonio Spurs']}
 
 teams['Conference'] = ''
 teams['Division'] = ''
@@ -58,17 +58,19 @@ for team in teams.index:
         teams.at[team, 'Conference'] = 'Western'
 
 
-team_div = teams[['Team Code', 'Division']]
+team_div = teams[['Team Code', 'Division', 'TeamID']]
 
 # Add team divisions in the main dataframe
 for year in years[:-3]:
     # Add Visitor/Neutral Division
     df[year] = df[year].merge(team_div, left_on='Visitor/Neutral', right_on='Team')
-    df[year].rename(columns={'Division': 'V/N Division', 'Team Code': 'V/N Code'}, inplace=True)
+    df[year].rename(columns={'Division': 'V/N Division', 'Team Code': 'V/N Code',
+                             'TeamID': 'V/N ID'}, inplace=True)
 
     # Add Home/Visitor Division
     df[year] = df[year].merge(team_div, left_on='Home/Neutral', right_on='Team')
-    df[year].rename(columns={'Division': 'H/N Division', 'Team Code': 'H/N Code'}, inplace=True)
+    df[year].rename(columns={'Division': 'H/N Division', 'Team Code': 'H/N Code',
+                             'TeamID': 'H/N ID'}, inplace=True)
     
     # Sort by Date
     df[year].sort_values(by=['Date'], inplace=True)
@@ -85,12 +87,12 @@ for year in years[-3:]:
     # Add Visitor/Neutral Division
     df[year] = df[year].merge(team_div_code, left_on='Visitor/Neutral', right_on='Team Code')
     df[year].rename(columns={'Division': 'V/N Division', 'Visitor/Neutral': 'V/N Code',
-                             'Team': 'Visitor/Neutral'}, inplace=True)
+                              'Team': 'Visitor/Neutral', 'TeamID': 'V/N ID'}, inplace=True)
 
     # Add Home/Visitor Division
     df[year] = df[year].merge(team_div_code, left_on='Home/Neutral', right_on='Team Code')
     df[year].rename(columns={'Division': 'H/N Division', 'Home/Neutral': 'H/N Code',
-                             'Team': 'Home/Neutral'}, inplace=True)
+                              'Team': 'Home/Neutral', 'TeamID': 'H/N ID'}, inplace=True)
     
     # Sort by Date
     df[year].sort_values(by=['Date'], inplace=True)
